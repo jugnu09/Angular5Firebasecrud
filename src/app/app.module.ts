e8c388e482833b61d7da9343e939e015bb1460c7
Ritesh <Ritesh@Riteshs-MacBook-Air.local>
2018-08-05 14:48:53 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { environment} from '../environments/environment';
+import { ToastrModule} from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp (environment.fireBaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig: {
+    apiKey: 'AIzaSyDZC3CN5Kok5dWrZOH5i9leT_FakQPOxYY',
+    authDomain: 'angular5firebasecrud-b2322.firebaseapp.com',
+    databaseURL: 'https://angular5firebasecrud-b2322.firebaseio.com',
+    projectId: 'angular5firebasecrud-b2322',
+    storageBucket: '',
+    messagingSenderId: '116041694920'
+    }
 };
 
 /*
